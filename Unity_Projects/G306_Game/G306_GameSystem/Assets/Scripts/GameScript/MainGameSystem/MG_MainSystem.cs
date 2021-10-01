f11728757d0228d758a45e061dbf8c02523f9b66
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2021-10-01 12:46:45 +0900

    基本システムのアップデート/タイトルUI変更 / メインゲームシステムアップデート

--- Unity_Projects/G306_Game/G306_GameSystem/Assets/Scripts/GameScript/MainGameSystem/MG_MainSystem.cs
+++ Unity_Projects/G306_Game/G306_GameSystem/Assets/Scripts/GameScript/MainGameSystem/MG_MainSystem.cs
@@ -13,16 +13,18 @@ public class MG_MainSystem : MonoBehaviour
     [SerializeField] private GameObject GameOverUI;
     [SerializeField] private GameObject GameClearUI;
     //Text
-
+    [SerializeField] private Text CountDownText;
     //float
-
+    [SerializeField] private float Stage_Minus;
+    [SerializeField] private float Stage_StartCountDown;
+    [SerializeField] private float CD_Timer;
     //bool
-
+    public bool StopMinusHealth;
     //int
     private void Awake()
     {
-        _SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
-        _MusicController = GameObject.Find("MusicControllerObject").GetComponent<MusicController>();
+        //_SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
+        //_MusicController = GameObject.Find("MusicControllerObject").GetComponent<MusicController>();
         _HealthSystem = GameObject.Find("Player").GetComponent<MG_HealthSystem>();
         Invoke("PlayStageMusic", 0.1f);
     }
@@ -32,10 +34,10 @@ public class MG_MainSystem : MonoBehaviour
         GameClearUI.SetActive(false);
     }
 
-
     void Update()
     {
-
+        MainGame();
+        isGameOver();
     }
 
     void isGameOver()
@@ -43,7 +45,6 @@ public class MG_MainSystem : MonoBehaviour
         if (_HealthSystem.isDead) //プレイヤーがゲームオーバー判定になったら
         {
             GameOverUI.SetActive(true);
-            
         }
     }
 
@@ -54,6 +55,57 @@ public class MG_MainSystem : MonoBehaviour
 
     void PlayStageMusic()
     {
+
+    }
+
+    void MainGame()
+    {
+        if (_HealthSystem.isStart == false) //ゲームが開始する前
+        {
+            if(CD_Timer > 1.0f)
+            {
+                Stage_StartCountDown -= 1.0f;
+                CD_Timer = 0.0f;
+            }
+            else if(CD_Timer < 1.0f)
+            {
+                CD_Timer += Time.deltaTime;
+            }
+
+            switch (Stage_StartCountDown) //ステージ開始カウントダウン表示
+            {
+                case 0.0f:
+                    Stage_StartCountDown = 0.0f;
+                    CountDownText.text = "ステージ開始...!";
+                    Invoke("DeleteCDText", 1.0f);
+                    break;
+                case 1.0f:
+                    CountDownText.text = "1";
+                    break;
+                case 2.0f:
+                    CountDownText.text = "2";
+                    break;
+                case 3.0f:
+                    CountDownText.text = "3";
+                    break;
+                case 4.0f:
+                    CountDownText.text = "4";
+                    break;
+                case 5.0f:
+                    CountDownText.text = "5";
+                    break;
+            }
+        }
+        else if(_HealthSystem.isStart == true && StopMinusHealth == false)//ゲームが開始したら
+        {
+            _HealthSystem.MinusHealth = Stage_Minus; //ステージごとに減少率を選べる
+        }
+    }
+
+    void DeleteCDText()
+    {
+        CountDownText.text = "";
+        _HealthSystem.isStart = true;
 
     }
 }

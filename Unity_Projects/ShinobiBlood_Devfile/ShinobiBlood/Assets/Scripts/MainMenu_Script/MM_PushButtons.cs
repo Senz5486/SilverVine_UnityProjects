5720e56ce69b9652596d4ad15b3f083019fc2aa3
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2021-10-10 03:43:54 +0900

    Change File name / ビルドしたものを追加

---
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/MainMenu_Script/MM_PushButtons.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MM_PushButtons : MonoBehaviour
+{
+    //Class
+    OR_SceneManager _SceneManager;
+    //GameObjects
+    [SerializeField] private GameObject MainMenuUI;
+    [SerializeField] private GameObject TutorialUI;
+    [SerializeField] private GameObject StageSelectUI;
+    [SerializeField] private GameObject OptionUI;
+    [SerializeField] private GameObject ConfirmExit;
+
+    //bool
+    public bool isFirstPlay;
+
+    void Start()
+    {
+        _SceneManager = this.GetComponent<OR_SceneManager>();
+    }
+
+    public void BacktoMainMenu()
+    {
+        MainMenuUI.SetActive(true);
+        StageSelectUI.SetActive(false);
+        OptionUI.SetActive(false);
+        ConfirmExit.SetActive(false);
+        TutorialUI.SetActive(false);
+    }
+    public void PushGameStart()
+    {
+        if (isFirstPlay == true) //èââÒÉvÉåÉCÇæÇ¡ÇΩèÍçá
+        {
+            TutorialUI.SetActive(true);
+            MainMenuUI.SetActive(false);
+            
+        }
+        else if (isFirstPlay == false) //ìÒâÒñ⁄à»ç~ÇÃèÍçá
+        {
+            StageSelectUI.SetActive(true);
+            MainMenuUI.SetActive(false);
+        }
+    }
+
+    public void PushOption()
+    {
+        OptionUI.SetActive(true);
+        MainMenuUI.SetActive(false);
+    }
+
+    public void PushExit()
+    {
+        MainMenuUI.SetActive(false);
+        ConfirmExit.SetActive(true);
+    }
+    public void PushTutorialPlay()
+    {
+        _SceneManager.SceneName = "Stage_Tutorial";
+        _SceneManager.NextSceneLoad();
+        isFirstPlay = false;
+    }
+    public void PushConfirmExit()
+    {
+        Application.Quit();
+    }
+}

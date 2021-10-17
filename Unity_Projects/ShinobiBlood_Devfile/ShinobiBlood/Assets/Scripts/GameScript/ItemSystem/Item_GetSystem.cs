f39679bb9f9d269f40eccde24765016ac99ef7bc
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2021-10-18 02:55:27 +0900

    一部にNamespaceを付け忘れてたので修正

--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/ItemSystem/Item_GetSystem.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/ItemSystem/Item_GetSystem.cs
@@ -3,100 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class Item_GetSystem : MonoBehaviour
+namespace Senz_Program
 {
-    PlayerController _playerController;
-    MG_HealthSystem _mghealthsystem;
-    SoundController _SoundController;
-    Tutorial_MainSystem _tutorial;
-    string Item_Name;        //アイテム名
-    public int Item_Type;           //アイテムタイプ (1: 回復アイテム/2:速度上昇アイテム)
-    float Item_HealPower;           //アイテム1のヒールパワー
-    float Item_SpeedPower;          //アイテム2の速度上昇率
-    public float isSpeedTime;       //アイテム2の速度上昇時間
-    [SerializeField] private TextMesh Item_DisplayName = null;
-    private void Awake()
+    public class Item_GetSystem : MonoBehaviour
     {
-        _SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
-        if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
+        PlayerController _playerController;
+        MG_HealthSystem _mghealthsystem;
+        SoundController _SoundController;
+        Tutorial_MainSystem _tutorial;
+        string Item_Name;        //アイテム名
+        public int Item_Type;           //アイテムタイプ (1: 回復アイテム/2:速度上昇アイテム)
+        float Item_HealPower;           //アイテム1のヒールパワー
+        float Item_SpeedPower;          //アイテム2の速度上昇率
+        public float isSpeedTime;       //アイテム2の速度上昇時間
+        [SerializeField] private TextMesh Item_DisplayName = null;
+        private void Awake()
         {
-            _tutorial = GameObject.Find("ScriptObject").GetComponent<Tutorial_MainSystem>();
+            _SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
+            if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
+            {
+                _tutorial = GameObject.Find("ScriptObject").GetComponent<Tutorial_MainSystem>();
+            }
+            _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+            _mghealthsystem = GameObject.Find("Player").GetComponent<MG_HealthSystem>();
         }
-        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
-        _mghealthsystem = GameObject.Find("Player").GetComponent<MG_HealthSystem>();
-    }
-     void Update()
-    {
-        switch (Item_Type)
-        {
-            case 1://回復アイテムの場合
-                Item_Name = "ZOK-2";
-                Item_DisplayName.text = Item_Name;
-                Item_HealPower = 10.0f;
-                Item_SpeedPower = 0.0f;
-                isSpeedTime = 0.0f;
-                break;
-            case 2://速度上昇アイテムの場合
-                Item_Name = "KSK-5";
-                Item_DisplayName.text = Item_Name;
-                Item_SpeedPower = 2.0f;
-                isSpeedTime = 3.0f;
-                Item_HealPower = 0.0f;
-                break;
-        }
-    }
-    private void OnTriggerEnter(Collider Player)
-    {//オブジェクトがPlayerかどうかを1度の判別で済むように、かつDestroyの場所も１か所に
-        if(Player.gameObject.tag == "Player")//プレイヤーに触れたとき
+        void Update()
         {
             switch (Item_Type)
             {
                 case 1://回復アイテムの場合
-                    _mghealthsystem.TokenDamage(-Item_HealPower);
-                    _SoundController.PlaySEAudio = 0;
-                    if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
-                    {
-                        _tutorial.isGetHealItem = true;
-                    }
-                    if (_mghealthsystem.CurrentHealth >= _mghealthsystem.MaxHealth)//体力最大値補正 
-                    {
-                        _mghealthsystem.CurrentHealth = _mghealthsystem.MaxHealth;
-                        _mghealthsystem.TokenDamage(-_mghealthsystem.MaxHealth);
-                    }
+                    Item_Name = "ZOK-2";
+                    Item_DisplayName.text = Item_Name;
+                    Item_HealPower = 10.0f;
+                    Item_SpeedPower = 0.0f;
+                    isSpeedTime = 0.0f;
                     break;
-                case 2://加速アイテムの場合
-                    _playerController.SpeedItemPower = 3;
-                    _playerController.AccelerationSpeed = true;
-                    _SoundController.PlaySEAudio = 0;
-                    if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
-                    {
-                        _tutorial.isGetSpeedItem = true;
-                    }
+                case 2://速度上昇アイテムの場合
+                    Item_Name = "KSK-5";
+                    Item_DisplayName.text = Item_Name;
+                    Item_SpeedPower = 2.0f;
+                    isSpeedTime = 3.0f;
+                    Item_HealPower = 0.0f;
                     break;
             }
-            Destroy(this.gameObject);//アイテム消去
         }
+        private void OnTriggerEnter(Collider Player)
+        {//オブジェクトがPlayerかどうかを1度の判別で済むように、かつDestroyの場所も１か所に
+            if (Player.gameObject.tag == "Player")//プレイヤーに触れたとき
+            {
+                switch (Item_Type)
+                {
+                    case 1://回復アイテムの場合
+                        _mghealthsystem.TokenDamage(-Item_HealPower);
+                        _SoundController.PlaySEAudio = 0;
+                        if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
+                        {
+                            _tutorial.isGetHealItem = true;
+                        }
+                        if (_mghealthsystem.CurrentHealth >= _mghealthsystem.MaxHealth)//体力最大値補正 
+                        {
+                            _mghealthsystem.CurrentHealth = _mghealthsystem.MaxHealth;
+                            _mghealthsystem.TokenDamage(-_mghealthsystem.MaxHealth);
+                        }
+                        break;
+                    case 2://加速アイテムの場合
+                        _playerController.SpeedItemPower = 3;
+                        _playerController.AccelerationSpeed = true;
+                        _SoundController.PlaySEAudio = 0;
+                        if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
+                        {
+                            _tutorial.isGetSpeedItem = true;
+                        }
+                        break;
+                }
+                Destroy(this.gameObject);//アイテム消去
+            }
 
 
-        //if (Player.gameObject.tag == "Player" && Item_Type == 1) //回復アイテムに触れた時
-        //{
-        //    _mghealthsystem.CurrentHealth += Item_HealPower;
-        //    if(_mghealthsystem.CurrentHealth >= _mghealthsystem.MaxHealth)
-        //    {
-        //        _mghealthsystem.CurrentHealth = _mghealthsystem.MaxHealth;
-        //    }
+            //if (Player.gameObject.tag == "Player" && Item_Type == 1) //回復アイテムに触れた時
+            //{
+            //    _mghealthsystem.CurrentHealth += Item_HealPower;
+            //    if(_mghealthsystem.CurrentHealth >= _mghealthsystem.MaxHealth)
+            //    {
+            //        _mghealthsystem.CurrentHealth = _mghealthsystem.MaxHealth;
+            //    }
 
-        //    Destroy(this.gameObject);//触れた時のこのオブジェクトを破壊する <--- 最終処理
-        //}
+            //    Destroy(this.gameObject);//触れた時のこのオブジェクトを破壊する <--- 最終処理
+            //}
 
-        //if (Player.gameObject.tag == "Player" && Item_Type == 2) //速度上昇アイテムに触れた時
-        //{
+            //if (Player.gameObject.tag == "Player" && Item_Type == 2) //速度上昇アイテムに触れた時
+            //{
 
-        //    _playerController.SpeedItemPower = Item_SpeedPower;
-        //    _playerController.SpeedItemTime += isSpeedTime;
-        //    Destroy(this.gameObject);//触れた時のこのオブジェクトを破壊する <--- 最終処理
-        //} 
+            //    _playerController.SpeedItemPower = Item_SpeedPower;
+            //    _playerController.SpeedItemTime += isSpeedTime;
+            //    Destroy(this.gameObject);//触れた時のこのオブジェクトを破壊する <--- 最終処理
+            //} 
 
+        }
     }
 }
--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/TutorialSystem/Tutorial_MainSystem.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/TutorialSystem/Tutorial_MainSystem.cs
@@ -4,134 +4,137 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Tutorial_MainSystem : MonoBehaviour
+namespace Senz_Program
 {
-    //Class
-    MusicController _MusicController;
-    SoundController _SoundController;
-    PlayerController _pc;
-    PlayerCamera _playerCamera;
-    MG_HealthSystem _MG_HealthSystem;
-    MG_MainSystem _MG_MainSystem;
-    //Gameobject
-    [SerializeField] private GameObject TutorialUI;
-    [SerializeField] private GameObject[] CheckPoint;
-    [SerializeField] private Text TutorialTipsText;
+    public class Tutorial_MainSystem : MonoBehaviour
+    {
+        //Class
+        MusicController _MusicController;
+        SoundController _SoundController;
+        PlayerController _pc;
+        PlayerCamera _playerCamera;
+        MG_HealthSystem _MG_HealthSystem;
+        MG_MainSystem _MG_MainSystem;
+        //Gameobject
+        [SerializeField] private GameObject TutorialUI;
+        [SerializeField] private GameObject[] CheckPoint;
+        [SerializeField] private Text TutorialTipsText;
 
-    [SerializeField] private GameObject[] ColObject;
-    [SerializeField] private GameObject Player;
-    [SerializeField] private GameObject TutorialHealItem;
-    [SerializeField] private GameObject TutorialSpeedItem;
-    //[SerializeField] private GameObject TutorialSpeedItem;
-    //bool
-    bool isCanMove;
-    bool UpdateOnce_Heal;
-    bool UpdateOnce_Speed;
-    public bool isGetHealItem;
-    public bool isGetSpeedItem;
-    private void Awake()
-    {
-        isGetHealItem = false;
-        isGetSpeedItem = false;
-        UpdateOnce_Heal = false;
-        UpdateOnce_Speed = false;
-        _MG_HealthSystem = GameObject.Find("Player").GetComponent<MG_HealthSystem>();
-        _MG_MainSystem = GameObject.Find("ScriptObject").GetComponent<MG_MainSystem>();
-        ColObject[0].SetActive(true);
-        _pc = GameObject.Find("Player").GetComponent<PlayerController>();
-        _playerCamera = GameObject.Find("ScriptObject").GetComponent<PlayerCamera>();
-        _SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
-        _MusicController = GameObject.Find("MusicControllerObject").GetComponent<MusicController>();
-    }
-    void Start()
-    {
-        if(SceneManager.GetActiveScene().name == "Stage_Tutorial")
+        [SerializeField] private GameObject[] ColObject;
+        [SerializeField] private GameObject Player;
+        [SerializeField] private GameObject TutorialHealItem;
+        [SerializeField] private GameObject TutorialSpeedItem;
+        //[SerializeField] private GameObject TutorialSpeedItem;
+        //bool
+        bool isCanMove;
+        bool UpdateOnce_Heal;
+        bool UpdateOnce_Speed;
+        public bool isGetHealItem;
+        public bool isGetSpeedItem;
+        private void Awake()
         {
-            _MusicController.PlayBGMAudio = 5;
-            TutorialUI.SetActive(true);
-            TutorialTipsText.text = "訓練場へようこそ";
+            isGetHealItem = false;
+            isGetSpeedItem = false;
+            UpdateOnce_Heal = false;
+            UpdateOnce_Speed = false;
+            _MG_HealthSystem = GameObject.Find("Player").GetComponent<MG_HealthSystem>();
+            _MG_MainSystem = GameObject.Find("ScriptObject").GetComponent<MG_MainSystem>();
+            ColObject[0].SetActive(true);
+            _pc = GameObject.Find("Player").GetComponent<PlayerController>();
+            _playerCamera = GameObject.Find("ScriptObject").GetComponent<PlayerCamera>();
+            _SoundController = GameObject.Find("SoundControllerObject").GetComponent<SoundController>();
+            _MusicController = GameObject.Find("MusicControllerObject").GetComponent<MusicController>();
+        }
+        void Start()
+        {
+            if (SceneManager.GetActiveScene().name == "Stage_Tutorial")
+            {
+                _MusicController.PlayBGMAudio = 5;
+                TutorialUI.SetActive(true);
+                TutorialTipsText.text = "訓練場へようこそ";
+                isCanMove = false;
+                Invoke("Tips1", 2.0f);
+            }
+        }
+
+        private void Update()
+        {
+            _pc.EnableCharaSystem = isCanMove;
+            if (isGetHealItem && !UpdateOnce_Heal)
+            {
+                Invoke("Tips5", 0.5f);
+                UpdateOnce_Heal = true;
+            }
+            if (isGetSpeedItem && !UpdateOnce_Speed)
+            {
+                Invoke("Tips8", 0.5f);
+                UpdateOnce_Speed = true;
+            }
+        }
+
+        void Tips1()
+        {
+            TutorialTipsText.text = "A,← キーで左移動 / D,→ キーで右移動 / スペース,↑ キー でジャンプ が出来ます";
+            isCanMove = true;
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips2", 10.0f);
+        }
+        void Tips2()
+        {
             isCanMove = false;
-            Invoke("Tips1", 2.0f);
+            _MG_HealthSystem.isStart = true;
+            _MG_MainSystem.Stage_Minus = 1.42f;
+            TutorialTipsText.text = "このゲージは『体力』です　　時間が経過するごとに減少します";
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips3", 7.0f);
         }
-    }
-
-    private void Update()
-    {
-        _pc.EnableCharaSystem = isCanMove;
-        if(isGetHealItem && !UpdateOnce_Heal)
+        void Tips3()
         {
-            Invoke("Tips5", 0.5f);
-            UpdateOnce_Heal = true;
+            isCanMove = false;
+            _MG_HealthSystem.isStart = false;
+            ColObject[0].SetActive(false);
+            _playerCamera.TargetObject = TutorialHealItem.transform;
+            TutorialTipsText.text = "これは『ZOK-2』  触れてみてください";
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips4", 3.5f);
         }
-        if(isGetSpeedItem && !UpdateOnce_Speed)
+        void Tips4()
         {
-            Invoke("Tips8", 0.5f);
-            UpdateOnce_Speed = true;
+            isCanMove = true;
+            _playerCamera.TargetObject = Player.transform;
+            TutorialTipsText.text = "";
         }
-    }
-
-    void Tips1()
-    {
-        TutorialTipsText.text = "A,← キーで左移動 / D,→ キーで右移動 / スペース,↑ キー でジャンプ が出来ます";
-        isCanMove = true;
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips2", 10.0f);
-    }
-    void Tips2()
-    {
-        isCanMove = false;
-        _MG_HealthSystem.isStart = true;
-        _MG_MainSystem.Stage_Minus = 1.42f;
-        TutorialTipsText.text = "このゲージは『体力』です　　時間が経過するごとに減少します";
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips3", 7.0f);
-    }
-    void Tips3()
-    {
-        isCanMove = false;
-        _MG_HealthSystem.isStart = false;
-        ColObject[0].SetActive(false);
-        _playerCamera.TargetObject = TutorialHealItem.transform;
-        TutorialTipsText.text = "これは『ZOK-2』  触れてみてください";
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips4", 3.5f);
-    }
-    void Tips4()
-    {
-        isCanMove = true;
-        _playerCamera.TargetObject = Player.transform;
-        TutorialTipsText.text = "";
-    }
-    void Tips5()
-    {
-        isCanMove = false;
-        TutorialTipsText.text = "『ZOK-2』は『体力』を回復させます";
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips6", 5.0f);
-    }
-    void Tips6()
-    {
-        _playerCamera.TargetObject = TutorialSpeedItem.transform;
-        TutorialTipsText.text = "これは『KSK-5』  触れてみてください";
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips7", 3.5f);
-    }
-    void Tips7()
-    {
-        isCanMove = true;
-        _playerCamera.TargetObject = Player.transform;
-        TutorialTipsText.text = "";
-    }
-    void Tips8()
-    {
-        TutorialTipsText.text = "KSK-5の説明をここに書く";
-        _SoundController.PlaySEAudio = 7;
-        Invoke("Tips9", 5.0f);
-    }
-    void Tips9()
-    {
-        isCanMove = false;
-        TutorialTipsText.text = "次にからくりの説明を開始します";
-        _SoundController.PlaySEAudio = 7;
+        void Tips5()
+        {
+            isCanMove = false;
+            TutorialTipsText.text = "『ZOK-2』は『体力』を回復させます";
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips6", 5.0f);
+        }
+        void Tips6()
+        {
+            _playerCamera.TargetObject = TutorialSpeedItem.transform;
+            TutorialTipsText.text = "これは『KSK-5』  触れてみてください";
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips7", 3.5f);
+        }
+        void Tips7()
+        {
+            isCanMove = true;
+            _playerCamera.TargetObject = Player.transform;
+            TutorialTipsText.text = "";
+        }
+        void Tips8()
+        {
+            TutorialTipsText.text = "KSK-5の説明をここに書く";
+            _SoundController.PlaySEAudio = 7;
+            Invoke("Tips9", 5.0f);
+        }
+        void Tips9()
+        {
+            isCanMove = false;
+            TutorialTipsText.text = "次にからくりの説明を開始します";
+            _SoundController.PlaySEAudio = 7;
+        }
     }
 }

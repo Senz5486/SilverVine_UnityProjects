13df084ea58936a019bb1096973e4b5a19023f64
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2021-10-22 11:37:18 +0900

    ゲームクリア時のスコアシステム制作中

--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/ScoreResult.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/ScoreResult.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScoreResult : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Senz_Program {
+    public class ScoreResult : MonoBehaviour
     {
-        
-    }
+        Score_Mass MassScore;
+        [SerializeField] private Text ResultScoreText;
+        [SerializeField] private Text CurrentStageMaxScoreText;
+        private float ShownTime;
+        void Start()
+        {
+            MassScore = GameObject.Find("ScriptObject").GetComponent<Score_Mass>();
+            ShownTime = 3.0f;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            ShownTime -= Time.deltaTime;
+            if (ShownTime > 0)
+            {
+                ResultScoreText.text = Random.Range(100000, 999999).ToString("0");
+            }
+            else if (ShownTime < 0)
+            {
+                ResultScoreText.text = MassScore.Score().ToString();
+                ShownTime = -1;
+            }
+        }
     }
 }

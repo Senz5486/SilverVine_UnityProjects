5fabdf0176a0a5caca89db6b2f99136d15d63260
DESKTOP-G23FMHQ\senz <ryuuki285666@gmail.com>
2021-10-30 02:56:29 +0900

    セーブデータの初期値を書くところを間違えていたので修正

--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/Overrall_Script/OR_SaveSystem.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/Overrall_Script/OR_SaveSystem.cs
@@ -32,31 +32,6 @@ namespace Senz_Program
                 Debug.Log("ƒtƒ@ƒCƒ‹‚ªŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
                 SaveData = new SaveData();
                 Save();
-                //‰Šú’l
-                Instance.SaveData.Stage1_MaxScores = 0;
-                Instance.SaveData.Stage2_MaxScores = 0;
-                Instance.SaveData.Stage3_MaxScores = 0;
-                Instance.SaveData.Stage4_MaxScores = 0;
-                Instance.SaveData.Stage5_MaxScores = 0;
-                Instance.SaveData.StageEx_MaxScores = 0;
-
-                Instance.SaveData.RK_Stage1_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-                Instance.SaveData.RK_Stage2_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-                Instance.SaveData.RK_Stage3_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-                Instance.SaveData.RK_Stage4_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-                Instance.SaveData.RK_Stage5_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-                Instance.SaveData.RK_StageEx_PlayerNames = "Œ»İÅ‚‹L˜^‚ª‚ ‚è‚Ü‚¹‚ñ";
-
-                Instance.SaveData.PlayerName = "–¼‘O–³‚µ “o˜^‚µ‚Ä‚­‚¾‚³‚¢";
-
-                Instance.SaveData.SEVolume = 0.5f;
-                Instance.SaveData.BGMVolume = 0.4f;
-
-                Instance.SaveData.isFullScreen = true;
-                Instance.SaveData.ScreenWidth = 1920;
-                Instance.SaveData.ScreenHeight = 1080;
-                Instance.SaveData.ResolutionSetting = 5;
-                Instance.SaveData.VSyncIsEnable = true;
                 return;
             }
 
--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/Overrall_Script/SaveData.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/Overrall_Script/SaveData.cs
@@ -9,30 +9,30 @@ namespace Senz_Program
     public class SaveData
     {
         //int
-        public int ResolutionSetting; //解像度用の保存int
-        public int Stage1_MaxScores;   //スコア仮保存
-        public int Stage2_MaxScores;   //スコア仮保存
-        public int Stage3_MaxScores;   //スコア仮保存
-        public int Stage4_MaxScores;   //スコア仮保存
-        public int Stage5_MaxScores;   //スコア仮保存
-        public int StageEx_MaxScores;   //スコア仮保存
+        public int ResolutionSetting = 5; //解像度用の保存int
+        public int Stage1_MaxScores = 0;   //スコア仮保存
+        public int Stage2_MaxScores = 0;   //スコア仮保存
+        public int Stage3_MaxScores = 0;   //スコア仮保存
+        public int Stage4_MaxScores = 0;   //スコア仮保存
+        public int Stage5_MaxScores = 0;   //スコア仮保存
+        public int StageEx_MaxScores = 0;   //スコア仮保存
         //float
-        public int ScreenWidth;   //解像度用の保存float 
-        public int ScreenHeight;  //解像度用の保存float 
-        public float SEVolume;      //サウンドエフェクト用の保存float
-        public float BGMVolume;     //BGM用の保存float
+        public int ScreenWidth = 1920;   //解像度用の保存float 
+        public int ScreenHeight = 1080;  //解像度用の保存float 
+        public float SEVolume = 0.4f;      //サウンドエフェクト用の保存float
+        public float BGMVolume = 0.5f;     //BGM用の保存float
         //string
-        public string PlayerName;   //プレイヤー名用の保存String
-        public string RK_Stage1_PlayerNames;   //スコア仮保存
-        public string RK_Stage2_PlayerNames;   //スコア仮保存
-        public string RK_Stage3_PlayerNames;   //スコア仮保存
-        public string RK_Stage4_PlayerNames;   //スコア仮保存
-        public string RK_Stage5_PlayerNames;   //スコア仮保存
-        public string RK_StageEx_PlayerNames;   //スコア仮保存
+        public string PlayerName = "名前無し 登録してください";   //プレイヤー名用の保存String
+        public string RK_Stage1_PlayerNames = "現在最高記録がありません";   //スコア仮保存
+        public string RK_Stage2_PlayerNames = "現在最高記録がありません";   //スコア仮保存
+        public string RK_Stage3_PlayerNames = "現在最高記録がありません";   //スコア仮保存
+        public string RK_Stage4_PlayerNames = "現在最高記録がありません";   //スコア仮保存
+        public string RK_Stage5_PlayerNames = "現在最高記録がありません";   //スコア仮保存
+        public string RK_StageEx_PlayerNames = "現在最高記録がありません";   //スコア仮保存
         //bool
-        public bool VSyncIsEnable;  //VSync用の保存bool
-        public bool isFullScreen;   //フルスクリーン確認用の保存bool
-        public bool FinishTutorial; //チュートリアルを終えてるか確認用の保存bool
+        public bool VSyncIsEnable = true;  //VSync用の保存bool
+        public bool isFullScreen = true;   //フルスクリーン確認用の保存bool
+        public bool FinishTutorial = false; //チュートリアルを終えてるか確認用の保存bool
         
     }
 }

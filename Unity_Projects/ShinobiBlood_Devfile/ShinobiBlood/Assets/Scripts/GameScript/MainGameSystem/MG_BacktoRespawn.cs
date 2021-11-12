735cfd4e058090e3d9322d38a40b6247ffeb8fdc
B9336_Fukayama <B9336@oic.jp>
2021-11-12 17:41:48 +0900

    落下時に速度上昇が解除されるように修正。メインメニューに仮置きされたプレイヤを削除

--- Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/MainGameSystem/MG_BacktoRespawn.cs
+++ Unity_Projects/ShinobiBlood_Devfile/ShinobiBlood/Assets/Scripts/GameScript/MainGameSystem/MG_BacktoRespawn.cs
@@ -7,11 +7,13 @@ namespace Senz_Program
     public class MG_BacktoRespawn : MonoBehaviour
     {
         PlayerStatus _PlayerStatus;
+        PlayerController _playerController;
         private Transform PlayerObject;
         private Vector3 FirstSpawnPosition;
 
         void Start() { 
             _PlayerStatus = GameObject.Find("Player").GetComponent<PlayerStatus>();
+            _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
             PlayerObject = GameObject.Find("Player").GetComponent<Transform>();
             FirstSpawnPosition = PlayerObject.transform.position;
         }
@@ -20,6 +22,7 @@ namespace Senz_Program
         {
             if (other.gameObject.tag == "Player")
             {
+                _playerController.AccelerationSpeed = false;
                 PlayerObject.transform.position = FirstSpawnPosition;
                 _PlayerStatus.RespawnCount++;
             }
